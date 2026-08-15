5aaf52983836ad03d0e11b1f97764539b5f8d110
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-15 16:48:06 -0300

    Processa retorno do consentimento no Android

--- app/src/main/java/br/com/gerenciamentogastos/MainActivity.kt
+++ app/src/main/java/br/com/gerenciamentogastos/MainActivity.kt
@@ -1,18 +1,35 @@
 package br.com.gerenciamentogastos
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import br.com.gerenciamentogastos.ui.FinanceApp
 import br.com.gerenciamentogastos.ui.theme.GerenciamentoGastosTheme
 
 class MainActivity : ComponentActivity() {
+    private var callbackUri by mutableStateOf<Uri?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        callbackUri = intent?.data
         setContent {
             GerenciamentoGastosTheme {
-                FinanceApp()
+                FinanceApp(
+                    callbackUri = callbackUri,
+                    onCallbackHandled = { callbackUri = null }
+                )
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        callbackUri = intent.data
     }
 }
